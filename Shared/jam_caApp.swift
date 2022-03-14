8bf119a4c14e2166d57815e885d1d3ea9edc63a2
Lukáš Andrlík <lukas.andrlik@adastra.one>
2022-03-14 08:47:26 +0100

    Initial Commit

---
+++ Shared/jam_caApp.swift
@@ -0,0 +1,17 @@
+//
+//  jam_caApp.swift
+//  Shared
+//
+//  Created by Lukas Andrlik on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct jam_caApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
